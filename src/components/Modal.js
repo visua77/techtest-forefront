26d8cd0f9a05c31e5b7336a41a927d550fd37440
Niclas Binnquist <niclasbinnquist@niclas-binnquists-mac-pro.local>
2020-12-18 14:41:16 +0100

    styletweak modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,8 +3,12 @@ import React from 'react'
 const Modal = ({description, modaltoggle, handleModal})=> {
     return(
         <div className={modaltoggle ? 'modal-active' : 'modal'}onClick={handleModal}>
-            <div className="modal-wrapper"style={{backgroundImage: `url(https://image.tmdb.org/t/p/w780${description.img})`}}>          <h3>{description.title}</h3>
+            <div className="modal-wrapper"style={{backgroundImage: `url(https://image.tmdb.org/t/p/w780${description.img})`}}>    
+            <span>
+            <h3>{description.title}</h3>
             <p className="modal-descr">{description.description}</p>
+            </span>
+            <span className="close-wrapper"><span className="close-modal">X</span></span>
             </div>
         </div>
     )
